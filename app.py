095a7c7b2b165ec1d6709e21bafc42693a390477
Thilak <thilakdevaraj20@gmail.com>
2026-04-13 23:57:28 +0530

    fix ui without folder change

--- app.py
+++ app.py
@@ -60,13 +60,17 @@
 
 # if __name__ == "__main__":
 #     app.run(host="0.0.0.0", port=10000)
-from flask import Flask
+from flask import Flask, send_from_directory
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Deploy working ✅"
+    return send_from_directory(".", "index.html")  # 🔥 current folder serve
+
+@app.route("/<path:path>")
+def static_files(path):
+    return send_from_directory(".", path)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=10000)
